9e11133d1190ca902659a0bcb7f37844fd71bdef
Pjotters-Chain <05.hek_domein@icloud.com>
2025-05-01 01:12:33 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-  apiUrl: 'https://bt-dates-adware-bennet.trycloudflare.com', // backend API
+  apiUrl: 'https://instructor-mainstream-from-traffic.trycloudflare.com', // backend API
   riffusionUrl: 'https://winner-struggle-practical-catalogue.trycloudflare.com' // nieuwe Riffusion API
 };
 window.config = config;
