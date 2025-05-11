c42ec7b4baec7b350452af2552dbe4b569fbb7a8
Pjotters-Chain <05.hek_domein@icloud.com>
2025-05-11 10:45:31 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-  apiUrl: 'https://per-filling-secret-thread.trycloudflare.com', // backend API
-  riffusionUrl: 'https://highlighted-powers-usage-reads.trycloudflare.com' // nieuwe Riffusion API
+  apiUrl: 'https://passport-dialog-worker-result.trycloudflare.com', // backend API
+  riffusionUrl: 'https://athletic-scholar--already.trycloudflare.com' // nieuwe Riffusion API
 };
 window.config = config;
